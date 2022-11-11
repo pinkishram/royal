801e3ce1d6396b487869f50f15c297d630a1df0b
pinkishram <85747097+pinkishram@users.noreply.github.com>
2022-11-11 23:49:52 +0200

    change 2

--- assurance.js
+++ assurance.js
@@ -5,30 +5,58 @@ const text = "bring your idea for experts";
 indexText = 0;
 
 function writetext() {
-    textLine.innerText = text.slice(0, indexText);
+  textLine.innerText = text.slice(0, indexText);
 
-    if (indexText > text.length - 1) {
-        clearInterval(intertext);
-    }
-    indexText++;
+  if (indexText > text.length - 1) {
+    clearInterval(intertext);
+  }
+  indexText++;
 }
 const intertext = setInterval(writetext, 200);
 
 let barClick = document.querySelector(".fa-bars");
 
 barClick.addEventListener("click", () => {
-    document.querySelector(".hide").classList.toggle("hide-elm-s");
-    document.querySelector(".list-m").classList.toggle("show-list");
+  document.querySelector(".hide").classList.toggle("hide-elm-s");
+  document.querySelector(".list-m").classList.toggle("show-list");
 });
 
 // service page///////
 function mouseOverActive_s() {
-    serviceBoxS.classList.remove("trans-service-s");
-    serviceBoxS.classList.add("op-service-s");
+  serviceBoxS.classList.remove("trans-service-s");
+  serviceBoxS.classList.add("op-service-s");
 }
 
 function mouseOutActive_s() {
-    serviceBoxS.classList.add("trans-service-s");
-    serviceBoxS.classList.remove("op-service-s");
+  serviceBoxS.classList.add("trans-service-s");
+  serviceBoxS.classList.remove("op-service-s");
 }
 // service page///////
+
+// cabinets java /////////////////
+let direction = document.querySelector(".fas fa-chevron-down");
+
+// direction.addEventListener(function () {})
+
+let jSlide = document.querySelectorAll(".j-slide");
+let minus = document.querySelectorAll(".minus");
+jSlide[0].style.maxHeight = "160px";
+minus[0].classList.add("fa-plus-square");
+
+function resetExpand() {
+  for (let i = 0; i < jSlide.length; i++) {
+    jSlide[i].style.maxHeight = "0px";
+  }
+  for (let i = 0; i < jSlide.length; i++) {
+    minus[i].classList.remove("fa-plus-square");
+    minus[i].classList.add("fa-minus-square");
+  }
+}
+
+function expandquery(x) {
+  resetExpand();
+  jSlide[x - 1].style.maxHeight = "160px";
+  minus[x - 1].classList.toggle("fa-plus-square");
+}
+
+// cabinets java /////////////////
